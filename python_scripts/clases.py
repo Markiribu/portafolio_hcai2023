f5b239a1f0b8cbf2046c2a9c9217e79d267c6f8c
Marcos Eduardo Castillo Bugueño <marcoseduardo.castillo@userena.cl>
2023-09-28 15:03:59 -0300

    clase 11: instancia, definiciones fuera de la clase, etc

--- python_scripts/clases.py
+++ python_scripts/clases.py
@@ -1,11 +1,12 @@
 class Fraccion():
     def __init__(self, numerador, denominador):
+        if ((type(numerador)!=int) ^ (type(denominador)!=int)):
+            raise Exception("ERROR: numerador y denominador deben ser numeros enteros")
         self.n = numerador
         self.d = denominador
-    def __rep__(self):
-        self.print = str(n) + '' + str(d)
+    def show(self):
+        print(str(self.n)+'/'+str(self.d))
 
 x = Fraccion(1,2)
 
-print(x.n,'/',x.d)
-print(x)
+x.show()
---
+++ written_notes/class11.md
@@ -0,0 +1,11 @@
+# Clase 11: continuacion de clases
+### Los atributos son mutables, ende dado un objeto x de clase clase con atributos x1 x2, puedes definir algo fuera del mismo como x.x3
+#### Class testclase:
+#### 	def __init__(self, x1, x2):
+#### 		self.x1 = x1
+#### 		self.x2 = x2
+#### #------
+#### objeto = testclase("tumami","tupapi")
+#### objeto.x3 = "tuprimi"
+### ------------------------------------
+### 
