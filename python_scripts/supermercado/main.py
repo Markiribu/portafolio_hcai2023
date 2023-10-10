9afeca3e3fd38d8f30f26ded7e579d55ce8e43a8
Marcos Bugueño <marcoseduardo.castillo@userena.cl>
2023-10-10 10:44:23 -0300

    supermarket diseño

--- python_scripts/supermercado/main.py
+++ python_scripts/supermercado/main.py
@@ -22,6 +22,65 @@ class Validador(Persona):
 
 class Cliente(Persona):
     
-    def __init__(self):
-        self.buscfactor = 0
+    def __init__(self,buscfactor=200):
+        self.buscfactor = buscfactor
         self.search_time = 0
+
+    def set_target(nitems):
+        self.items = nitems
+        self.searchTIme = nitems*self.buscfactor
+
+    def buscar_items(self, nitems):
+        while(0 < dt and 0 < self.searchItem):
+            self.searchTime -= 1
+            dt -= 1
+        return dt
+
+    def comprar(self):
+        return self.items
+
+class Caja():
+    def __init__(self):
+        self.cajero = Validador
+        self.cola = []
+
+    def add_cliente(self, Cliente):
+        self.cola.append(Cliente)
+
+    def kick_client(self):
+        return 0
+
+    def trabajar(self):
+        return 0
+
+class Supermarket():
+    def __init__(self):
+        self.cajas = []
+        self.clientes_buscando = []
+        self.clientesTotal = []
+
+    def set_flujo(cps):
+        return 0
+
+    def __clientes_nuevos():
+        return 0
+
+    def __clientes_buscar():
+        return 0
+
+    def __asignar_cajas():
+        return 0
+
+    def run():
+        return 0
+
+class Simulacion:
+    def __init__(self):
+        local = Supermarket
+
+    def correr_simulacion():
+        return 0
+
+#-------------------------------------------
+
+print('Hermano se ve grandecito')
